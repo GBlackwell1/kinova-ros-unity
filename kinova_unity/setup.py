ab5b4b244b1029f50a25e21483dcb0141c527f6d
Gabriel Blackwell <76234706+GBlackwell1@users.noreply.github.com>
2024-04-04 18:29:51 -0400

    actionlib dep

--- kinova_unity/setup.py
+++ kinova_unity/setup.py
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['kinova_unity'],
     package_dir={'': 'nodes'},
-    requires=['std_msgs', 'rospy', 'kinova_msgs', 'kinova_driver', 'geometry_msgs']
+    requires=['std_msgs', 'rospy', 'kinova_msgs', 'kinova_driver', 'geometry_msgs', 'actionlib_msgs']
 )
 
 setup(**setup_args)
